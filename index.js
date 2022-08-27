7b4287f2a00e4e7bb4a7bdfc5bfec24e7a87f929
Mercy <mercycheruiyot2013@gmail.com>
2022-08-27 04:11:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main=document.getElementById('main').remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute ("id","victory");//setting the id to victory
+newHeader.innerHTML="Mercy is the champion";//adding the text
+document.body.appendChild(h1);//adding h1 to the body
